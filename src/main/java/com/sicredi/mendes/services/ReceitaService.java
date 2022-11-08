89131ba6de3679e1804d2ea96df2093730c14351
Matheus Mendes <mendesm13.m@gmail.com>
2022-11-08 16:27:12 -0300

    Descomenta thread.sleep

--- src/main/java/com/sicredi/mendes/services/ReceitaService.java
+++ src/main/java/com/sicredi/mendes/services/ReceitaService.java
@@ -36,8 +36,8 @@ public class ReceitaService {
         }
 
         // Simula tempo de resposta do serviço (entre 1 e 5 segundos)
-//        long wait = Math.round(Math.random() * 4000) + 1000;
-//        Thread.sleep(wait);
+        long wait = Math.round(Math.random() * 4000) + 1000;
+        Thread.sleep(wait);
 
         // Simula cenario de erro no serviço (0,1% de erro)
         long randomError = Math.round(Math.random() * 1000);
