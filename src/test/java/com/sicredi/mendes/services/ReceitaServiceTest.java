cc68e5a7f1bc5a374c673e42a5ba6f809b9e7917
Matheus Mendes <mendesm13.m@gmail.com>
2022-11-08 16:29:09 -0300

    Adicionando testes unitários para classe ReceitaService

--- src/test/java/com/sicredi/mendes/services/ReceitaServiceTest.java
+++ src/test/java/com/sicredi/mendes/services/ReceitaServiceTest.java
@@ -7,5 +7,36 @@ import org.junit.jupiter.api.Test;
 
 class ReceitaServiceTest {
 
+	String agenciaCerta = "0101";
+	String agenciaErrada = "010";
+	String conta = "12225-6";
+	double saldo = 100.00;
 
+	@Test
+	void contaDeveSerAtualizada() throws NumberFormatException, RuntimeException, InterruptedException {
+		ReceitaService receitaService = new ReceitaService();
+		Boolean atualizouConta = receitaService.atualizarConta(agenciaCerta, conta.replaceAll("-", ""), saldo, "P");
+		assertTrue(atualizouConta);
+	}
+
+	@Test
+	void agenciaErrada() throws NumberFormatException, RuntimeException, InterruptedException {
+		ReceitaService receitaService = new ReceitaService();
+		Boolean atualizouConta = receitaService.atualizarConta(agenciaErrada, conta.replaceAll("-", ""), saldo, "I");
+		assertFalse(atualizouConta);
+	}
+
+	@Test
+	void contaErrada() throws NumberFormatException, RuntimeException, InterruptedException {
+		ReceitaService receitaService = new ReceitaService();
+		Boolean atualizouConta = receitaService.atualizarConta(agenciaErrada, conta, saldo, "A");
+		assertFalse(atualizouConta);
+	}
+
+	@Test
+	void statusErrado() throws NumberFormatException, RuntimeException, InterruptedException {
+		ReceitaService receitaService = new ReceitaService();
+		Boolean atualizouConta = receitaService.atualizarConta(agenciaErrada, conta.replaceAll("-", ""), saldo, "C");
+		assertFalse(atualizouConta);
+	}
 }
